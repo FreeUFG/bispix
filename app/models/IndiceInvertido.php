383a14e9bc0f7402dbe0bb3f0a179de8f783f7a1
Daniel Leonardo (FnZ) <daniel.fnz@hotmail.com>
2015-06-23 13:12:12 -0300

    Insersao das consultas NOT,XOR,AND E OR e RANQUEAMENTO TF-IDF
    Fixes #12 Insersao das consultas NOT,XOR,AND E OR com consultas simples,
    e consultas compostas.
    Fixes #10 Ranqueamento TF-IDF.

--- app/models/IndiceInvertido.php
+++ app/models/IndiceInvertido.php
@@ -15,6 +15,7 @@ class IndiceInvertido extends Eloquent{
 		else if(self::count() != 0){
 			self::truncate();
 		}
+
 	}
 	public static function tokenizer($nomeDiretorio)
 	{
@@ -109,7 +110,7 @@ class IndiceInvertido extends Eloquent{
 		$simbolosRemocao =
 			array(
 				"?", "!", ",", ";", "(",
-				")", "\"", ":", "."
+				")", "'\'", ":", ".","-","%","º","\"","$","[","]"
 			);
 		$termoNormalizado = str_replace($simbolosRemocao, "", $termo);
 		$termoNormalizado = mb_strtolower($termoNormalizado);
@@ -118,11 +119,13 @@ class IndiceInvertido extends Eloquent{
 	}
 	public static function postings($query)
 	{
+		//Consulta simples de somente um termo na query
 		$postings = IndiceInvertido::select('documento')
-						->where('termo', $query)
-						->distinct()
-						->lists('documento');
+							->where('termo', $query)
+							->distinct()
+							->lists('documento');
 		return $postings;
+		break;
 	}
 	public static function parametros($nomeMetodo)
 	{
@@ -216,4 +219,234 @@ class IndiceInvertido extends Eloquent{
 
         return $data;
 	}
+
+	public static function consultaNOT($termo)
+	{
+		//Consulta DE TODOS OS DOCUMENTOS
+		$TodosDocumentos = IndiceInvertido::select('documento')
+							->distinct()
+							->lists('documento');	
+		//Consulta simples DE DOCUMENTOS com o termo						
+		$serNegado= self::postings($termo);	
+		
+		//Vai percorrer os 2 arrays, e quando encontrar o respectivo termo nos documentos, nao adiciona ele na lista
+		$p1 = 0;
+		$p2 = 0;
+		$count = count($serNegado);
+		$count2 = count($TodosDocumentos);
+		$result =array();
+		while($p1!=$count && $p2!=$count2) {
+			if($serNegado[$p1]==$TodosDocumentos[$p2]){
+					$p1++;
+					$p2++;
+				}
+			else if($serNegado[$p1]<$TodosDocumentos[$p2]){
+					array_push($result,$serNegado[$p1]);
+					$p1++;
+				}
+			else {
+					array_push($result,$TodosDocumentos[$p2]);
+					$p2++;
+				}
+			}	
+			if($p1!=$count){
+			while($p1!=$count) {
+			array_push($result,$serNegado[$p1]);
+			$p1++;
+			}	
+		}	
+		else if($p2!=$count2){
+			while($p2!=$count2) {
+			array_push($result,$TodosDocumentos[$p2]);
+			$p2++;
+			}	
+		}
+		return $result;
+	}
+	//Percorre os 2 arrays, adicionando os valores no ARRAY result
+	public static function consultaOR($arrayTermo1,$arrayTermo2)
+	{
+		$p1 = 0;
+		$p2 = 0;
+		$count = count($arrayTermo1);
+		$count2 = count($arrayTermo2);
+		$result =array();
+		while($p1!=$count && $p2!=$count2) {
+			if($arrayTermo1[$p1]==$arrayTermo2[$p2]){
+					array_push($result,$arrayTermo1[$p1]);
+					$p1++;
+					$p2++;
+				}
+			else if($arrayTermo1[$p1]<$arrayTermo2[$p2]){
+					array_push($result,$arrayTermo1[$p1]);
+					$p1++;
+				}
+			else {
+					array_push($result,$arrayTermo2[$p2]);
+					$p2++;
+				}
+		}
+		if($p1!=$count){
+			while($p1!=$count) {
+			array_push($result,$arrayTermo1[$p1]);
+			$p1++;
+			}	
+		}	
+		else if($p2!=$count2){
+			while($p2!=$count2) {
+			array_push($result,$arrayTermo2[$p2]);
+			$p2++;
+			}	
+		}	
+		return $result;
+	}
+
+	//Percorre os 2 ARRAYS, mais so adiciona no ARRAY result se os 2 termos conter no MESMO documento
+	public static function consultaAND($arrayTermo1,$arrayTermo2) {
+		$p1 = 0;
+		$p2 = 0;
+		$count = count($arrayTermo1);
+		$count2 = count($arrayTermo2);
+		$result =array();
+		while($p1!=$count && $p2!=$count2) {
+				if($arrayTermo1[$p1]==$arrayTermo2[$p2]){
+					array_push($result,$arrayTermo1[$p1]);
+					$p1++;
+					$p2++;
+				}
+				else if($arrayTermo1[$p1]<$arrayTermo2[$p2]){
+					$p1++;
+				}
+				else {
+					$p2++;
+				}
+			}
+		return $result;
+	}
+	//Percorre os 2 ARRAYS, mais adiciona no ARRAY result TODOS documentos onde nao contem o TERMO
+	public static function consultaXOR($arrayTermo1,$arrayTermo2) {		
+		$p1 = 0;
+		$p2 = 0;
+		$count = count($arrayTermo1);
+		$count2 = count($arrayTermo2);
+		$result =array();
+		while($p1!=$count && $p2!=$count2) {
+				if($arrayTermo1[$p1]==$arrayTermo2[$p2]){
+					$p1++;
+					$p2++;
+				}
+				else if($arrayTermo1[$p1]<$arrayTermo2[$p2]){
+					array_push($result,$arrayTermo1[$p1]);
+					$p1++;
+				}
+				else if($arrayTermo1[$p1]>$arrayTermo2[$p2]){
+					array_push($result,$arrayTermo2[$p2]);
+					$p2++;
+				}		
+			}
+	return $result;
+	}
+
+	private static function consultaDocumentoID($posicao){
+	//Consulta simples de um termo e retorna seu id
+		$postings = IndiceInvertido::select('documento')
+							->where('posicao', $posicao)
+							->distinct()
+							->lists('documento');
+		return $postings;
+		break;
+	}
+	
+	//Consulta todas posicoes do termo no banco;
+	public static function consultaPosicao($termo){
+		$postings = IndiceInvertido::select('posicao')
+		 					->where('termo', $termo)
+							->lists('posicao');
+
+		return $postings;
+		break;
+	}	
+	//Realiza a consulta por frase	
+	public static function consultaPorFrase($arrayPrimeiroTermo,$arraySegundoTermo) {
+		$p1 = 0;
+		$p2 = 0;
+		$count = count($arrayPrimeiroTermo);
+		$count2 = count($arraySegundoTermo);
+		$result =array();
+		while($p1!=$count & $p2!=$count2) {
+	
+				if($arrayPrimeiroTermo[$p1+1]==$arraySegundoTermo[$p2]){
+					array_push($result,$arraySegundoTermo[$p1]);
+					$p1++;
+					$p2++;
+				}
+				else if($arrayPrimeiroTermo[$p2]==$arraySegundoTermo[$p1]+1){
+				$p1++;
+				}
+				else {
+				$p2++;
+				}
+			}
+	
+		$resposta = self::consultaDocumentoID($result);
+		return $resposta;
+	}
+	//Consulta todos os termos no documento passado por referencia.
+	private static function consultaTermosDocumentoID($documento){
+		$postings = IndiceInvertido::select('termo')
+					->where('documento', $documento)
+					->distinct()
+					->lists('termo');
+					return $postings;
+	}
+
+	//Calcula o IDF, com BASE na QUANTIDADE TOTAL de arquivos da colecao, pela quantidade de arquivos que o termo aparece.
+	public static function valorIDF($QuantDocumentosComTermo)
+	{
+		if ($QuantDocumentosComTermo!=0) {
+			return log10(100/$QuantDocumentosComTermo);
+		}
+		return 0;
+	}
+
+	//RETORNA O VALOR TF-IDF 
+	private static function valorTF_IDF($TF,$IDF){
+		return $TF*$IDF;
+	}
+
+	//Consulta a TF no DOCUMENTO, e com isso ja calcula o TF_IDF de cada documento.
+	public static function consultaTF_IDF($termo,$documentos){	
+	//Converte o termo em minusculo
+	$termo=strtolower($termo);	
+	$frenquenciaTodos =array();
+
+	//Quantidade de documentos que contem a query
+	$DF=count($documentos);
+
+	//Realiza o calculo de IDF, de acordo com O DF;
+	$IDF=self::valorIDF($DF);
+
+	//Varre o array DOCUMENTOS,fazendo uma pesquisa DO TERMO em CADA DOCUMENTO.
+	foreach ($documentos as $documentoID) {
+		//Realiza a consulta de todos os termos no documento especifico..
+		$todosTermos =self::consultaTermosDocumentoID($documentoID);
+		//Se ja achou uma frequencia no documento, entao assumios que seja 1
+		$TF=1;
+				//Varre o array TODOTERMOS(DOCUMENTO ESPECIFICO) e encontra a frequencia do termo no documento.
+				foreach ($todosTermos as $value) {
+					if($value==$termo){
+					$TF++;
+					}
+
+				}	
+
+		//Realiza o calculo de TF-IDF, de acordo com o TF E IDF;
+		$TF_IDF =self::valorTF_IDF($TF,$IDF);
+
+	//Adiciona no array multidimencional. DocumentoID, e Peso do documento.
+	$frenquenciaTodos[]=array('Documento' => $documentoID,'Peso'=>$TF_IDF);
+	}
+	return $frenquenciaTodos;
+	}
+	
 }
