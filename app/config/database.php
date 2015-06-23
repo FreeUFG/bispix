a31a1e90b9c3d42d74cdc4e07eb2f8dca4de222c
hcb13 <hortensiacb@gmail.com>
2015-06-23 20:29:29 -0300

    grupo Ontologias

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'database'  => 'teste',
+			'database'  => 'trabalhoIA',
 			'username'  => 'user_ia',
 			'password'  => 'user_ia',
 			'charset'   => 'utf8',
