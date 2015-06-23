3fb2bb53a16a63fd3169658786c3ee36f1a4fcef
Daniel Leonardo (FnZ) <daniel.fnz@hotmail.com>
2015-06-23 12:36:58 -0300

    Generização automatica para indexar qualquer colecao.
    Fixes #5 Generização automatica  para indexar qualquer colecao, bastando
    a colecao apenas estar dentro da pasta data/colecoes/.

--- app/views/site/gerar-indice/passo-1/index.blade.php
+++ app/views/site/gerar-indice/passo-1/index.blade.php
@@ -2,14 +2,15 @@
 	<h2>Coleção</h2>
 
 	<p>Escolha os documentos a serem indexados.</p>
-
 	<div class="form-group">
 		<label for="colecao" class="col-sm-2 control-label">Coleção</label>
 		<div class="col-sm-6">
 			<select class="form-control" name="colecao">
-				<option value="santa">Santa Cruz</option>
-				<option value="teste">Coleção de Teste</option>
-				<option value="bebidas">Bebidas</option>
+			 @foreach ($lista as $id)
+			
+			 <option value="{{ $id }}">{{ $id }}</option>
+     
+        @endforeach
 			</select>
 			<br>
 		</div>
